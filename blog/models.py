b997a2f5247e83fbe23c869c546d8da2fc2899ce
Paulo Vinicius <pauloviniciustoledo@hotmail.com>
2024-03-19 17:40:42 -0300

    #03 Terceiro Checkpoint. Refactoring Página Inicial.

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from datetime import datetime
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
-    conteudo = models.TextField()
+    conteudo = RichTextUploadingField()
     data_publicacao = models.DateTimeField(default=datetime.now())
 
     def __str__(self):
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('posts/<int:post_id>/detalhe_post', views.detalhe_post, name='detalhe_post')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
