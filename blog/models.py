ebb429b9cb5c8c4660eff2aaa7638dd4b4e7b971
Paulo Vinicius <pauloviniciustoledo@hotmail.com>
2024-03-19 12:02:11 -0300

    #02 Segundo Checkpoint. Criação dos templates home e detalhes_post..

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ from datetime import datetime
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
-    subtitle = models.CharField(max_length=255)
+    subtitulo = models.CharField(max_length=255)
     conteudo = models.TextField()
     data_publicacao = models.DateTimeField(default=datetime.now())
+
+    def __str__(self):
+        return self.titulo
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('posts/<int:post_id>/detalhe_post', views.detalhe_post, name='detalhe_post')
 ]
